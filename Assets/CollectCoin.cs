df5048c8b1c62e86934b4201eacef71b8b371c85
Nikola Grigorov <nikolagrigorov3@gmail.com>
2019-12-07 12:11:52 +0100

    FullCommit
    First Demo

---
+++ Assets/CollectCoin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CollectCoin : MonoBehaviour
+{
+    public TextMeshProUGUI tmpScore;
+    private int Coins = 0;
+    // Start is called before the first frame update
+    void setCoins()
+    {
+        tmpScore.text = Coins.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            Coins++;
+            setCoins();
+            
+        }
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float speed = 20f, horMove = 0f;
+    bool jump = false, crouch = false;
+    public CharacterController2D myChar;
+    public Animator myAnimator;
+    public Joystick joystick;
+    // Update is called once per frame
+    void Update()
+    {
+        if (joystick.Horizontal >= .2f)
+        {
+            horMove = speed;
+        }else if (joystick.Horizontal <= -.2f)
+        {
+            horMove = -speed;
+        }
+        else
+        {
+            horMove = 0f;
+        }
+        myAnimator.SetFloat("Speed", Mathf.Abs(horMove));
+
+        float verticalMove = joystick.Vertical;
+
+        if(verticalMove >= .5f)
+        {
+            jump = true;
+            myAnimator.SetBool("isFlying", true);
+        }
+
+        if(verticalMove <= -.5f)
+        {
+            crouch = true;
+        }
+        else
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        myAnimator.SetBool("isFlying", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        myAnimator.SetBool("isCrouching", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        myChar.Move(horMove * Time.fixedDeltaTime,crouch, jump);
+        jump = false;
+        
+    }
+}
